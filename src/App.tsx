51be765bdec64dc020ef7022dd39d06135c05c8d
Kirill Rikshpun <kirillllrikshpun@example.com>
2022-11-28 09:37:14 -0800

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import Select from './Select';
+import Background from './Background';
+import './Background.css'
 import './App.css';
 
+type Data = {
+  name: string
+  email: string
+  format: string[]
+}
+
+const options = [
+    {label: `video`, value: `video`}, 
+    {label: `audio`, value: `audio`}
+  ]
+
 function App() {
+  const [data, setData] = useState<Data>({name: "", email: "", format: []})
+  const [open, setOpen] = useState<boolean>(false);
+
+  const onSelected = ( formatValue: string[]) => {
+    setData({...data, format: formatValue})
+  }
+
+  const print = () => {
+    return console.log(data)
+  }
+
+  console.log(data)
+
   return (
+    <>
+    <Background />
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+          <div className = "form-box">
+            <div className = 'input-box'>
+              <input className = 'input' placeholder = 'type your name...' name = "name" type = "text" onClick = {() => setOpen(false)} onChange = {(e) => {setData( {...data, name: e.target.value})}}/> 
+            </div>           
+            <div className = 'input-box'>
+              <input className = 'input' placeholder = 'type your email...' name = "email" type = "email" onClick = {() => setOpen(false)} onChange = {(e) => {setData( {...data, email: e.target.value})}}/>
+            </div>
+            <div className = 'input-box'>
+              <Select options = {options} open = {open} setOpen = {setOpen} onSelected = {onSelected} />
+            </div>
+            <div className = 'input-box'>
+              <input className = 'submitButton' type = "button" value = "submit" onClick = {() => print()}/>
+            </div>
+          </div>
+      </form>
     </div>
+    </>
+    
   );
 }
 
